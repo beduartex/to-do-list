d505ff04b17a292a50f3245c6ffdc63bf7fd88ca
Bernardo Duarte <97202284+beduartex@users.noreply.github.com>
2024-03-25 17:37:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const tarefaToDo = $('input').val();
+        const novaTarefa = $(`<li>${tarefaToDo}</li>`);
+
+        $(novaTarefa).appendTo('ul');
+        $(novaTarefa).fadeIn(750);
+        $('input').val('')
+    }) 
+    $('ul').on('click', 'li', function(){
+        $(this).toggleClass('strike').fadeOut(200);
+    })
+})
